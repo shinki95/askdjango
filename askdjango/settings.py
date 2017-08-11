6ab7da5e98800d986fb85930fa52feaca30362a9
Yunki <shinki95@gmail.com>
2017-08-11 13:38:40 +0900

    static으로 수정

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'askdjango', 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
